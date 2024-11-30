2c9560fc9c4e6e3f86993cdbd7a2fddf8a18d810
Achinit99 <achinit720@gmail.com>
2024-11-30 18:37:58 +0530

    create form

--- addStudent.php
+++ addStudent.php
@@ -3,4 +3,13 @@
   <head>
     <title>Add Student</title>
   </head>
+  <body>
+    <h2>Add Student</h2>
+
+    <form>
+      <label for="name">Name: </label>
+      <input type="text" name="name" id="name"><br><br>
+    </form>
+
+  </body>
 </html>
