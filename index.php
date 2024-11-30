a4adde5483209368b9fc6598b12459ff13d765bb
Achinit99 <achinit720@gmail.com>
2024-11-30 17:31:25 +0530

    create table

--- index.php
+++ index.php
@@ -3,4 +3,15 @@
   <head>
     <title>Besic PHP CRUD Website</title>
   </head>
+  <body>
+    <h1>Student Details</h1>
+    <table>
+      <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>City</th>
+        <th>Age</th>
+      </tr>
+    </table>
+  </body>
 </html>
