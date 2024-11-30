86b370ec1ff74464b77eb6a4248295bbf6aaefb1
Achinit99 <achinit720@gmail.com>
2024-11-30 18:41:52 +0530

    create form

--- addStudent.php
+++ addStudent.php
@@ -9,6 +9,14 @@
     <form>
       <label for="name">Name: </label>
       <input type="text" name="name" id="name"><br><br>
+
+      <label for="city">City: </label>
+      <input type="text" name="city" id="city"><br><br>
+
+      <label for="age">Age: </label>
+      <input type="number" name="age" id="age"><br><br>
+
+      <input type="submit" name="register" value="Register">
     </form>
 
   </body>
